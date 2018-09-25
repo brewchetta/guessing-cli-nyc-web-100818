07262a7651709428a81a03aa00481dc20193bd95
Chett Tiller <chett.tiller@gmail.com>
2018-09-25 22:05:21 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   input = ""
   while input != number
     number = rand(1..10).to_s
-    puts "Please guess a number!"
+    puts "Guess a number between 1 and 6"
     input = gets.chomp
     break if input.downcase == "exit"
     puts "The computer guessed #{number}." if input != number
