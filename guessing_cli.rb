c0cd981e03878160c4a51af2f0fc67be0ac922ef
Chett Tiller <chett.tiller@gmail.com>
2018-09-25 22:01:56 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,11 +1,12 @@
 def run_guessing_game
-  number = 0
+  number = ""
   input = ""
   while input != number
-    number = rand(1..10)
+    number = rand(1..10).to_s
     puts "Please guess a number!"
     input = gets.chomp
     break if input == "exit"
     puts "Sorry, it was #{number}! Guess again or type 'exit'" if input != number
   end
+  if input = number
 end
