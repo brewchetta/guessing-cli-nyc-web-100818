f632e9b467fbc22b7631ad6e3ebfc7fb49d58c4a
Chett Tiller <chett.tiller@gmail.com>
2018-09-25 22:11:08 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,7 +1,7 @@
 def run_guessing_game
   number = "1"
   input = ""
-  while input != number
+  while input != exit
     number = rand(1..6).to_s
     puts "Guess a number between 1 and 6."
     input = gets.chomp
